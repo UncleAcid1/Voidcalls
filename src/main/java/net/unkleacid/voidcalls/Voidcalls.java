4f1a4801b5626455cb8f43339092a2177b014622
telvarost <telontelvarost@gmail.com>
2025-07-07 20:48:05 -0400

    Add gitignore; Update to latest StationAPI and update mod dev environment; Ensure game tick event would run on client (still need to fully fix this)

--- src/main/java/net/unkleacid/voidcalls/Voidcalls.java
+++ src/main/java/net/unkleacid/voidcalls/Voidcalls.java
@@ -1,22 +1,25 @@
 package net.unkleacid.voidcalls;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.client.event.render.entity.EntityRendererRegisterEvent;
 import net.modificationstation.stationapi.api.event.entity.EntityRegister;
 import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
+import net.modificationstation.stationapi.api.event.tick.GameTickEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 import net.minecraft.item.Item;
+import net.unkleacid.voidcalls.client.VoidCallsClient;
 import net.unkleacid.voidcalls.entity.NotextureEntity;
 import net.unkleacid.voidcalls.entity.renderer.NotextureEntityRenderer;
-import net.unkleacid.voidcalls.entity.model.NotextureEntityModel;
 
 public class Voidcalls {
 
     @Entrypoint.Namespace
-    public static final Namespace NAMESPACE = Null.get();
+    public static Namespace NAMESPACE = Null.get();
 
     public static Item WEDDING_RING;
 
@@ -35,5 +38,12 @@ public class Voidcalls {
     @EventListener
     public void registerEntityRenderer(EntityRendererRegisterEvent event) {
         event.renderers.put(NotextureEntity.class, new NotextureEntityRenderer());
+    }
+
+    @EventListener
+    public void onGameTick(GameTickEvent event) {
+        if (EnvType.CLIENT == FabricLoader.getInstance().getEnvironmentType()) {
+            VoidCallsClient.onClientGameTick(event);
+        }
     }
 }
