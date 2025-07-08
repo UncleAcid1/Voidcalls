962d50c69063d1f3198cc97b4505e8cea6f94092
kiki <hamh3m@gmail.com>
2025-07-08 08:55:36 -0400

    remove last bits of code from pixel girls

--- src/main/java/net/unkleacid/voidcalls/Voidcalls.java
+++ src/main/java/net/unkleacid/voidcalls/Voidcalls.java
@@ -1,30 +1,14 @@
 package net.unkleacid.voidcalls;
 
+import net.unkleacid.voidcalls.entity.NotextureEntity;
+import net.unkleacid.voidcalls.entity.renderer.NotextureEntityRenderer;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.client.event.render.entity.EntityRendererRegisterEvent;
 import net.modificationstation.stationapi.api.event.entity.EntityRegister;
-import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
-import net.modificationstation.stationapi.api.template.item.TemplateItem;
-import net.modificationstation.stationapi.api.util.Namespace;
-import net.modificationstation.stationapi.api.util.Null;
-import net.minecraft.item.Item;
-import net.unkleacid.voidcalls.entity.NotextureEntity;
-import net.unkleacid.voidcalls.entity.renderer.NotextureEntityRenderer;
 
+@Entrypoint
 public class Voidcalls {
-
-    @Entrypoint.Namespace
-    public static Namespace NAMESPACE = Null.get();
-
-    public static Item WEDDING_RING;
-
-    @EventListener
-    public void registerItems(ItemRegistryEvent event) {
-        WEDDING_RING = new TemplateItem(NAMESPACE.id("wedding_ring"))
-                .setMaxCount(1)
-                .setTranslationKey(NAMESPACE, "wedding_ring");
-    }
 
     @EventListener
     public void registerEntities(EntityRegister event) {
